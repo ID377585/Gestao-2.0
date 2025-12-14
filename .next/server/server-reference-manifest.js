fc63ce4f9fd216f948b8dfd26fdd6f68027c5f84
ID377585 <144333117+ID377585@users.noreply.github.com>
2025-12-14 18:39:30 -0300

    fix: resolve all shadcn/ui dependencies and stable build

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"0NdsilQNnJLnslKd3rkayZqF3VIwjIa42aj6CbQx1EA=\"}"
---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/photos/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/workspace-*/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
